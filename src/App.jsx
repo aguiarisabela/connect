e499c8d9446e6baea841f69f1a554b0872dac2fb
Isabela Aguiar <isilav.is54@gmail.com>
2026-02-11 19:35:56 -0300

    ✨feat: adicionando o toastify

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import Voluntariado from "./pages/voluntariado/Voluntariado";
 import Mentoria from "./pages/mentoria/Mentoria";
 import Eventos from "./pages/eventos/Eventos";
 import Usuario from "./pages/user/User";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -22,6 +24,7 @@ const App = () => {
           <Route path="/eventos" element={<Eventos />} />
           <Route path="/usuario" element={<Usuario />} />
         </Routes>
+        <ToastContainer position="top-right" autoClose={3000} />
         <Footer />
       </BrowserRouter>
     </>
--- src/components/infocard/Card.jsx
+++ src/components/infocard/Card.jsx
@@ -1,6 +1,21 @@
+import { toast } from 'react-toastify';
 import * as S from "./card.module.scss";
 
 export const Card = ({ image, alt, title, text, span, button }) => {
+  
+  // Função que dispara o alerta
+  const notify = () => {
+    toast.success("Inscrito com sucesso!", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "light",
+    });
+  }
+
   return (
     <article className={S.card}>
       <div className={S.card__image}>
@@ -9,7 +24,10 @@ export const Card = ({ image, alt, title, text, span, button }) => {
       <h2 className={S.card__title}>{title}</h2>
       <p className={S.card__text}>{text}</p>
       <span className={S.card__text}>{span}</span>
-      <button className={S.card__button}>{button}</button>
+
+      <button className={S.card__button} onClick={notify}>
+        {button}
+      </button>
     </article>
   );
-};
+}
